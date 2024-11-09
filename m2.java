f137a56ecf380dada3765c140ac7465cb4877335
Michale0808 <aug.mic.10@wp.pl>
2024-11-09 08:30:26 +0100

    Update m2.java

--- m2.java
+++ m2.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello michal!");
-    }
-}
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj liczbe1");
+        int number = input.nextInt();
 
+        System.out.println("Podaj liczbe2");
+        int number2 = input.nextInt();
+
+        System.out.println("Podaj liczbe3");
+        int number3 = input.nextInt();
+
+
+            int maximum = Math.max(Math.max(number2, number3), number);
+        int minimum = Math.min(Math.min(number2, number3), number);
+        System.out.println("Podaj liczbe najmniejsza:" + minimum + "Podaj liczbe najwieksza:" + maximum);
+            }
+        }
